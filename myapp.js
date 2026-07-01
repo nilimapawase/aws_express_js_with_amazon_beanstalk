a69c875b0b46dbc343d43760237f3b43cb4c950d
nilimapawase <nilimapawase14@gmail.com>
2026-07-01 12:09:19 +0530

    run app.js instaed of myapp.js

--- myapp.js
+++ myapp.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const mysql = require("mysql2");
+// const express = require("express");
+// const mysql = require("mysql2");
 
-const app = express();
+// const app = express();
 
-const PORT = process.env.PORT || 9595;
+// const PORT = process.env.PORT || 9595;
 
-const con = mysql.createConnection({
-    host: "tuesdaydb.cpkqm80282t1.ap-south-1.rds.amazonaws.com",
-    user: "admin",
-    password: "Nilima#2927",
-    database: "companydb",
-    port: 3309
-});
+// const con = mysql.createConnection({
+//     host: "tuesdaydb.cpkqm80282t1.ap-south-1.rds.amazonaws.com",
+//     user: "admin",
+//     password: "Nilima#2927",
+//     database: "companydb",
+//     port: 3309
+// });
 
-con.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Database Connected");
-    }
-});
+// con.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Database Connected");
+//     }
+// });
 
-app.get("/api/student", (req, res) => {
-    con.query("SELECT * FROM students", (err, result) => {
-        if (err) return res.status(500).send(err);
-        res.json(result);
-    });
-});
+// app.get("/api/student", (req, res) => {
+//     con.query("SELECT * FROM students", (err, result) => {
+//         if (err) return res.status(500).send(err);
+//         res.json(result);
+//     });
+// });
 
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+// app.listen(PORT, () => {
+//     console.log("Server running on port " + PORT);
+// });
